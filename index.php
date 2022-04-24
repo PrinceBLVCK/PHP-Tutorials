be01fd32e8385af066c1b80403cd7016a9b80cf7
Prince <princekarabo70@gmail.com>
2022-04-24 13:21:41 +0200

    creating tut structure
    also uploaded database files

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <h1 style="text-align: center;">PHP Tutorials</h1>
+
+    <ul>
+        <li><a href="">Intro</a></li>
+        <li><a href="">Basic PHP</a></li>
+        <li><a href="">Advanced Arrays</a></li>
+        <li><a href="">Funtions</a></li>
+        <li><a href="">File Systems</a></li>
+        <li><a href="database/index.php">mysql Database</a></li>
+        <li><a href="">Cookies and Sessions</a></li>
+    </ul>
+</body>
+</html>
